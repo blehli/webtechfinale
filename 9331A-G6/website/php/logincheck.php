f5828053995299dde834f7d066111e553ee35f40
Jhaylord <2163058@slu.edu.ph>
2018-05-01 10:26:51 +0800

    added initial login script

---
+++ 9331A-G6/website/php/logincheck.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+if (isset($_POST['submit'])){
+    include 'dbconn.php';
+    $username = mysqli_real_escape_string($conn, $_POST['uname']);
+    $password = mysqli_real_escape_string($conn, $_POST['pass']);
+
+    //error handlers
+    //check if inputs are empty
+    if(empty($username) || empty($password)){
+        header("Location: ../index.html?login=empty");
+        exit();
+    }else {
+        $sql ="SELECT * FROM users WHERE uname = '$username'";
+        $result = mysqli_query($conn,$sql);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck < 1){
+            header("Location: ../index.html?login=error");
+            exit();
+        } else{
+            if($row = mysqli_fetch_assoc($result)){
+                //De-hashing the password
+                $hashedPwdCheck = password_verify($password, $row['pass']);
+                if($hashedPwdCheck == false){
+                    header("Location: ../index.html?login=error");
+                    exit();
+                }elseif($hashedPwdCheck == true){
+                    //login user here
+                    $_SESSION['u_id'] = $row['user_id'];
+                    $_SESSION['u_first'] = $row['fname'];
+                    $_SESSION['u_last'] = $row['lname'];
+                    $_SESSION['u_name'] = $row['uname'];
+                    $_SESSION['u_email'] = $row['email'];
+                     header("Location: ../index.html?login=success");
+                    exit();
+                }
+
+            }
+        }
+
+    }
+} else {
+    header("Location: ../index.html?submit=error");
+    exit();
+}
+
+?>
